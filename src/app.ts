b70f68bba22ec97ade41ca64dbe2143a854ede81
YutoUrushima <yuto259233@gmail.com>
2023-02-16 17:10:01 +0900

    Add private word

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,13 +9,28 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    // validations
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accountding = new Department('Accounting');
 // console.log(accountding);
 
+accountding.addEmployee('Max');
+accountding.addEmployee('Manu');
+
 accountding.describe();
+accountding.printEmployeeInformation();
 
-const accountdingCopy = {name: 'DUMMY', describe: accountding.describe};
+// const accountdingCopy = {name: 'DUMMY', describe: accountding.describe};
 
-accountdingCopy.describe();
+// accountdingCopy.describe();
+
