1ec073fd683e1da1ee193c60522d7dd34022d9b6
YutoUrushima <yuto259233@gmail.com>
2023-02-27 17:51:59 +0900

    Add Generics

--- src/app.ts
+++ src/app.ts
@@ -1,148 +1,34 @@
-type Admin = {
-  name: string;
-  priviledge: string[];
+// const names: Array<string> = [];
+
+// const promise: Promise<string> = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Done')
+//   }, 2000)
+// });
+
+// promise.then(data => {
+//   data.split(' ');
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
 }
 
-type Employee = {
-  name: string;
-  startDate: Date;
+console.log(merge({name: 'max'}, {age: 30}));
+
+const mergedObj = merge<{name: string}, {age: number}>({name: 'max'}, {age: 30});
+console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
 }
 
-// interface ElevatedEmployee extends Admin, Employee {}
-
-type ElevatedEmployee = Admin & Employee;
-
-const e1: ElevatedEmployee = {
-  name: 'Max',
-  priviledge: ['create-server'],
-  startDate: new Date(),
-}
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
-
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b == "string") {
-    return a.toString() + b.toString();
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'no value';
+  if (element.length > 0) {
+    descriptionText = 'value is ' + element.length;
   }
-  return a + b;
+  return [element, descriptionText];
 }
 
-const result = add('Hello', 'TypeScript');
-result.split(' ');
-
-const fetchedUserData = {
-  id: 'u1',
-  name: 'user1',
-  job: {
-    title: 'Developer',
-    description: 'TypeScript'
-  },
-}
-
-console.log(fetchedUserData?.job?.title);
-
-const userInput = '';
-
-const storedData = userInput ?? 'DEFAULT';
-
-
-
-// type UnknownEmployee = Employee | Admin;
-
-// function printEmployeeInformation(emp: UnknownEmployee) {
-//   console.log(emp.name);
-//   if ('priviledge' in emp) {
-//     console.log("Priviledge: " + emp.priviledge );
-//   }
-//   if ('startDate' in emp) {
-//     console.log("Start Date: " + emp.startDate );
-//   }
-// }
-
-// printEmployeeInformation({name: "Manu", startDate: new Date()});
-
-// class Car {
-//   drive() {
-//     console.log("driving now...");
-//   }
-// }
-
-// class Truck {
-//   drive() {
-//     console.log("driveing Truck now...");
-//   }
-
-//   loadCargo(amount: number) {
-//     console.log("loading load" + amount);
-//   }
-// }
-
-// type Vehicle = Car | Truck;
-
-// const v1 = new Car();
-// const v2 = new Truck();
-
-// function useVehicle(vehicle: Vehicle) {
-//   vehicle.drive();
-//   if(vehicle instanceof Truck) {
-//     vehicle.loadCargo(1000);
-//   }
-// }
-
-// useVehicle(v1);
-// useVehicle(v2);
-
-// interface Bird {
-//   type: 'bird',
-//   flyingSpeed: number;
-// }
-
-// interface Horse {
-//   type: 'horse',
-//   runningSpeed: number;
-// }
-
-// type Animal = Bird | Horse;
-
-// function moveAnimal(animal: Animal) {
-//   // if ("flyingSpeed" in animal) {
-//   //   console.log(animal.flyingSpeed);
-//   // }
-//   let speed;
-//   switch(animal.type) {
-//     case 'bird':
-//       speed = animal.flyingSpeed;
-//       break;
-//     case 'horse':
-//       speed = animal.runningSpeed;
-//       break;
-//   }
-//   console.log('Speed: ' + speed);
-// }
-
-// moveAnimal({type: 'bird', flyingSpeed: 10});
-
-// // const paragraph = document.getElementById('message-output');
-// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-// const userInputElement = document.getElementById('user-input');
-
-// if(userInputElement) {
-//   (userInputElement as HTMLInputElement).value = "Hello";
-// }
-
-// interface ErrorContainer {
-//   [prop: string]: string
-// }
-
-// const errorBag: ErrorContainer = {
-//   email: 'Invalid Email',
-//   userName: 'Invalid userName',
-// };
+console.log(countAndDescribe('Hello'));
