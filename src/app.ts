9cff88b3912b4ff66839c71743758e7f3bc62e9b
YutoUrushima <yuto259233@gmail.com>
2023-03-20 17:19:56 +0900

    Finish Decolator

--- src/app.ts
+++ src/app.ts
@@ -120,16 +120,47 @@ button.addEventListener('click', p.showMessage);
 
 // ---
 
-function Required() {
-
+interface ValidatorConfig {
+  [prop: string]: {
+    [validatableProp: string]: string[]
+  }
 }
 
-function PositiveNumber() {
-  
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) { 
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required'],
+  }
 }
 
-function validate(obj: object) {
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  }
+}
 
+let isValid = true;
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name]
+  if (!objValidatorConfig) {
+    return true;
+  }
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
 }
 
 class Course {
