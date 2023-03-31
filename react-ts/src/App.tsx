7c308461148b370ba44a9eae6248625de971b531
YutoUrushima <yuto259233@gmail.com>
2023-03-31 16:20:03 +0900

    Process prj

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model'
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}
+    ]);
+  };
   return (
     <div className="App">
-      
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
